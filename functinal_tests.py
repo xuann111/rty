a5294db1d0b97b07fb9c8fa732a885fadad1f9c1
ankang peng <peng1253854861@outlook.com>
2024-06-10 16:37:00 +0800

    message 6: Functional test now checks we can input a to-do item

--- functinal_tests.py
+++ functinal_tests.py
@@ -1,37 +1,62 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
+from selenium.webdriver.common.by import By
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTests(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome()
-
+        self.browser.implicitly_wait(3)
+    
     def tearDown(self):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # 张三听说有一个在线待办事项的应用
-        # 他去看了这个应用的首页
+
+        #张三听说有一个在线待办事项的应用
+        #他去看了这个应用的首页
         self.browser.get('http://localhost:8000')
 
-        # 他注意到网页里包含“To-Do”这个次
-        self.assertIn('To-Do', self.browser.title), "browser title was:" + self.browser.title
-        self.fail('Finish the test！')
+        #他注意到网页里包含“To-Do”这个词
+        self.assertIn('To-Do ', self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('To-Do ', header_text)
 
-        # 他按了回车键后，页面更新了
-        # 待办事项表格中显示了“1：Buy flowers”
+        #应用有一个输入待办事项的文本输入框
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+            )
 
-        # 页面中有显示了一个文本输入框，可以输入其他待办事项
-        # 他输入了“Send a gift to Lisi”
+        #他在文本输入框中输入了“Buy flowers”
+        inputbox.send_keys('Buy flowers')
+        
+        #他按了回车键键后，页面更新了
+        #待办事项表格中显示了“1：Buy flowers”
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-        # 页面再次更新，她的清单中显示了这两个待办事项
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_element(By.TAG_NAME, 'tr')
+        self.assertIn('1: Buy flowers', [rows.text for row in rows])
 
-        # 张三想知道这个网站是否会记住他的清单
-        # 他看到网站为他生成了一个唯一的URL
+        input("Press Enter to close the browser and finish the test...")
 
-        # 他访问那个URL，发现他的待办实现列表还在
-        # 他满意度离开了
 
-        if __name__ == '__main__':
-            unittest.main()
+        #页面中又显示了一个文本输入框，可以输入其他待办事项
+        #他输入了“Send a gift to girlfriend”
+        self.fail('Finish the test!')
+
+        #页面再次更新，她的清单中显示了这两个待办事项
+
+        #张三想知道这个网站是否会记住他的清单
+        #他看到网站为他生成了一个唯一的URL
+
+        #他访问那个URL，发现他的待办事项列表还在
+        #他满意的离开了
+if __name__ == '__main__':
+    unittest.main()        
