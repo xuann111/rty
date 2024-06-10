15c660b511140f03a941d84b9d21b5b612638c8a
ankang peng <peng1253854861@outlook.com>
2024-06-11 01:31:41 +0800

    message18: new URL and view for adding to existing lists. UT and FT pass

--- lists/models.py
+++ lists/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
-class List(models
-	.Model):
+class List(models.Model):
 	pass
-class Item(models
-	.Model):
+	
+class Item(models.Model):
     text = models.TextField(default='')
     list = models.ForeignKey(List, on_delete=models.CASCADE, default=None)
 # Create your models here.
