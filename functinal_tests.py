5ffaf63fc8e4a234ce3a70a8bcb8145634fee483
ankang peng <peng1253854861@outlook.com>
2024-06-10 08:39:00 +0800

    message 5: The first iteractive developmen

--- functinal_tests.py
+++ functinal_tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 import unittest
 
+
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
@@ -10,32 +11,27 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-
         # 张三听说有一个在线待办事项的应用
         # 他去看了这个应用的首页
         self.browser.get('http://localhost:8000')
 
-        #他注意到网页里包含“To-Do”这个次
+        # 他注意到网页里包含“To-Do”这个次
         self.assertIn('To-Do', self.browser.title), "browser title was:" + self.browser.title
-        self.fail('Finish the test')
+        self.fail('Finish the test！')
 
-# 应用有一个输入待办事项的文本输入框
+        # 他按了回车键后，页面更新了
+        # 待办事项表格中显示了“1：Buy flowers”
 
-# 他在文本输入框中输入了“Buy flowers”
+        # 页面中有显示了一个文本输入框，可以输入其他待办事项
+        # 他输入了“Send a gift to Lisi”
 
-# 他按了回车键后，页面更新了
-# 待办事项表格中显示了“1：Buy flowers”
+        # 页面再次更新，她的清单中显示了这两个待办事项
 
-# 页面中有显示了一个文本输入框，可以输入其他待办事项
-# 他输入了“Send a gift to Lisi”
+        # 张三想知道这个网站是否会记住他的清单
+        # 他看到网站为他生成了一个唯一的URL
 
-# 页面再次更新，她的清单中显示了这两个待办事项
+        # 他访问那个URL，发现他的待办实现列表还在
+        # 他满意度离开了
 
-# 张三想知道这个网站是否会记住他的清单
-# 他看到网站为他生成了一个唯一的URL
-
-# 他访问那个URL，发现他的待办实现列表还在
-# 他满意度离开了
-
-if __name__ == '__main__':
-	unittest.main()
+        if __name__ == '__main__':
+            unittest.main()
--- lists/tests.py
+++ lists/tests.py
@@ -1,10 +1,18 @@
 from django.test import TestCase
 from django.urls import resolve
-from lists.views import home_page #(1)
+from lists.views import home_page
+from django.http import HttpRequest
 
 class HomePageTest(TestCase):
 
-	def test_root_url_resolves_to_home_page_view(self):
-		found = resolve('/') #(2)
-		self.assertEqual(found.func,home_page) #(3)
-# Create your tests here.
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_return_correct_html(self):
+        request = HttpRequest()    #(1)
+        response = home_page(request)    #(2)
+        html = response.content.decode('utf8')    #（3）
+        self.assertTrue(html.startswith('<html>'))    #(4)
+        self.assertIn('<title>To-Do lists</title>', html)    #(5)
+        self.assertTrue(html.endswith('</html>'))    #(4)
