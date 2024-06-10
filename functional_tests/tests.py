b895a13e844c820125dee82f30765ec6d14bf068
ankang peng <peng1253854861@outlook.com>
2024-06-10 19:47:18 +0800

    message 13: refactor sleep()

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,7 +3,9 @@ from selenium.webdriver.common.keys import Keys
 import time
 from selenium.webdriver.common.by import By
 from django.test import LiveServerTestCase
+from selenium.common.exceptions import WebDriverException
 
+MAX_WAIT = 10
 
 class NewVisitorTests(LiveServerTestCase):
 
@@ -13,10 +15,18 @@ class NewVisitorTests(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, 'id_list_table')    #(3)
+                rows = table.find_elements(By.TAG_NAME, 'tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return     #(4)
+            except (AssertionError, WebDriverException) as e:    #(5)
+                if time.time() - start_time > MAX_WAIT:    #(6)
+                    raise e    #(6)
+                time.sleep(0.5)    #(5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
 
@@ -42,19 +52,18 @@ class NewVisitorTests(LiveServerTestCase):
         #他按了回车键键后，页面更新了
         #待办事项表格中显示了“1：Buy flowers”
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Buy flowers')
+        self.wait_for_row_in_list_table('1: Buy flowers')
 
         #页面中又显示了一个文本输入框，可以输入其他待办事项
         #他输入了“Give a gift to Lisi”
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         inputbox.send_keys('Give a gift to Lisi')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+
 
         #页面再次更新，她的清单中显示了这两个待办事项
-        self.check_for_row_in_list_table('1: Buy flowers')
-        self.check_for_row_in_list_table('2: Give a gift to Lisi')
+        self.wait_for_row_in_list_table('1: Buy flowers')
+        self.wait_for_row_in_list_table('2: Give a gift to Lisi')
 
         #张三想知道这个网站是否会记住他的清单
         #他看到网站为他生成了一个唯一的URL
